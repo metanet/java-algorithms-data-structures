42239ba4a888dc62f3ce4f2b7e549f02168a5e91
adite39 <54745477+adite39@users.noreply.github.com>
2020-10-03 15:21:51 +0530

    Create ReverseBits.java

---
+++ src/problems/leetcode/ReverseBits.java
@@ -0,0 +1,12 @@
+public class Solution {
+    /* https://leetcode.com/problems/reverse-bits/ */
+    public int reverseBits(int n) {
+        int result = 0;
+        for(int i=0;i<32;i++){
+            result<<=1;
+            result+=n%2;
+            n>>>=1;
+        }
+        return result;
+    }
+}
