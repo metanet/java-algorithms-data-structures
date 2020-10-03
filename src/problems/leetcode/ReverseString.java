e977ce1feb3d70401d72ff62a162fa0960402fca
ayushijain22 <48030391+ayushijain22@users.noreply.github.com>
2020-10-03 15:00:45 +0530

    Update ReverseString.java

--- src/problems/leetcode/ReverseString.java
+++ src/problems/leetcode/ReverseString.java
@@ -5,19 +5,16 @@ package problems.leetcode;
  */
 public class ReverseString {
 
-    public static void reverseString(char[] s) {
-        if (s == null || s.length < 2) {
+    public static void reverseString(String s) {
+        if (s == null || s.length() < 2) {
             return;
         }
 
-        int i = 0, j = s.length - 1;
+        int i = 0, j = s.length() - 1;
         while (i < j) {
-            char c = s[i];
-            s[i] = s[j];
-            s[j] = c;
-            i++;
-            j--;
+            s=s.charAt(j);
         }
+        return s;
     }
 
 }
